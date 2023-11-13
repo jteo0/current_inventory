3ff3e12d359dec4ddaf2208a6163ab217ff18099
jteo0 <114756781+jteo0@users.noreply.github.com>
2023-11-13 13:39:08 +0700

    overlap

--- lib/item_object.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ItemObject {
-  final String name;
-  final int amount;
-  final String description;
-
-  ItemObject(this.name, this.amount, this.description);
-}
